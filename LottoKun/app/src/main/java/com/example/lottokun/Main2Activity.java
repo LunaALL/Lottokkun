0f7cea0076f8213aba4fe1630e6dc9fb67bcd16b
Lunall <wooju407@naver.com>
2020-07-05 19:42:32 +0900

    dd

--- LottoKun/app/src/main/java/com/example/lottokun/Main2Activity.java
+++ LottoKun/app/src/main/java/com/example/lottokun/Main2Activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },4000);
 
 
 
